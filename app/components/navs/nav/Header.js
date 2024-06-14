0321b5602d1763cd4484c0fa13d6358b395e51fe
Ibidapo Ayomide <ibidapoayomide754@gmail.com>
2024-06-14 04:35:40 -0700

    Dropdown components added

--- app/components/navs/nav/Header.js
+++ app/components/navs/nav/Header.js
@@ -4,28 +4,28 @@ import React, { useContext } from "react";
 import { FiCloudOff, FiMenu, FiSearch } from "react-icons/fi";
 import Image from "next/image";
 import ModeToggler from "../../theme/Toggler";
+import NavMenu from "../components/NavMenu";
 
 const Header = () => {
   const { sideBarHandler } = useContext(globalContext);
   const [openSideBar, setOpenSideBar] = sideBarHandler;
   return (
-    <div className="w-full sm:px-20 px-4 flex items-center justify-between py-2 border-b border-b-gray-200">
-      <div className="sm:w-3/4 flex items-center space-x-3 md:space-x-8">
+    <div className="w-full sm:px-20 px-4 grid grid-cols-[auto,1fr,auto] md:grid-cols-[auto,auto,1fr,auto] items-center justify-between border-b border-b-gray-200">
+      <div className="w-[50px] flex items-center space-x-3 md:space-x-8">
         <FiMenu
           className="text-2xl cursor-pointer dark:text-white"
           onClick={() => setOpenSideBar(true)}
         />
-        <img src="/next.svg" className="w-20 h-20 md:w-28 md:h-28" alt="Blog Logo" />
-
-        <ul className="hidden md:flex flex-row space-x-5 mt-3">
-          {["News", "How To & Reviews", "Stories", "Contact Us"].map((menu, index)=>(
-            <li key={index} className={`text-md tracking-tight dark:text-white cursor-pointer`}>{menu}</li>
-          ))}
-        </ul>
       </div>
-      <div className="w-1/4 flex justify-end items-center space-x-2 md:space-x-5">
-           <ModeToggler />
-           <FiSearch className="cursor-pointer" />
+    <div className="w-full md:w-[100px] flex justify-center items-center">
+    <img src="/next.svg" className="w-20 h-20 md:w-28 md:h-28" alt="Blog Logo" />
+    </div>
+      <div className="md:flex hidden">
+      <NavMenu />
+      </div>
+      <div className="w-[100px] flex justify-end items-center space-x-2 md:space-x-5">
+        <ModeToggler />
+        <FiSearch className="cursor-pointer dark:text-white" />
       </div>
     </div>
   );
--- app/components/navs/sidebar/SideBar.js
+++ app/components/navs/sidebar/SideBar.js
@@ -1,9 +1,10 @@
-"use client"
+"use client";
 import { globalContext } from "../../../context/globalContext";
 import Image from "next/image";
 import React, { useContext } from "react";
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import Offcanvas from "react-bootstrap/Offcanvas";
 import { FiInstagram, FiLinkedin } from "react-icons/fi";
+import NavMenu from "../components/NavMenu";
 
 const SideBar = () => {
   const { sideBarHandler } = useContext(globalContext);
@@ -15,24 +16,33 @@ const SideBar = () => {
     <Offcanvas show={openSideBar} onHide={handleClose}>
       <Offcanvas.Header className="border-b border-b-gray-200" closeButton>
         <Offcanvas.Title>
-        <img src="/next.svg" className="w-20 h-20" alt="Blog Logo" />
+          <img src="/next.svg" className="w-20 h-20" alt="Blog Logo" />
         </Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body className="space-y-5">
-      <div className="flex">
-      <input type="text" className="outline-none focus:outline-none placeholder:font-normal placeholder:text-xs placeholder:tracking-normal border border-gray-20 px-2 py-3" placeholder="Enter keyword" />
-      <button className="text-center px-5 py-3 bg-blue-500 text-white">Search</button>
-      </div>
-      <div className="">
-        <div className="flex space-x-3 items-center">
-          <FiInstagram className="text-gray-600 text-md" />
-          <p className="text-black mt-3"> Instagram</p>
+        <div className="flex">
+          <input
+            type="text"
+            className="outline-none focus:outline-none placeholder:font-normal placeholder:text-xs placeholder:tracking-normal border border-gray-20 px-2 py-3"
+            placeholder="Enter keyword"
+          />
+          <button className="text-center px-5 py-3 bg-blue-500 text-white">
+            Search
+          </button>
         </div>
-        <div className="flex space-x-3 items-center">
-          <FiLinkedin className="text-blue-600 text-md" />
-          <p className="text-black mt-3 text-md"> Linkedin</p>
+       <div className="md:hidden flex">
+       <NavMenu />
+       </div>
+        <div className="">
+          <div className="flex space-x-3 items-center">
+            <FiInstagram className="text-gray-600 text-md" />
+            <p className="text-black mt-3"> Instagram</p>
+          </div>
+          <div className="flex space-x-3 items-center">
+            <FiLinkedin className="text-blue-600 text-md" />
+            <p className="text-black mt-3 text-md"> Linkedin</p>
+          </div>
         </div>
-      </div>
       </Offcanvas.Body>
     </Offcanvas>
   );
